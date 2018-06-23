f190a6a010a780d9c6e1546d91df818c04d1b781
Kevin Chen <kevinbc@gmail.com>
2018-06-23 13:18:46 -0700

    add simple logging

--- sync.py
+++ sync.py
@@ -1,4 +1,5 @@
 import common
+import datetime
 
 
 def sync(sc):
@@ -9,12 +10,14 @@ def sync(sc):
 
     # Sync newly added tracks.
     new_tracks = list(saved_tracks - playlist_all_tracks)
+    print(f'{len(new_tracks)} new tracks\nNew tracks: {new_tracks}')
     if len(new_tracks) > 0:
         sc.add_tracks_to_playlist(playlist_all_uri, new_tracks)
         sc.add_tracks_to_playlist(all_playlists['current'], new_tracks)
 
     # Sync removed tracks.
     removed_tracks = list(playlist_all_tracks - saved_tracks)
+    print(f'{len(removed_tracks)} removed tracks\nRemoved tracks: {removed_tracks}')
     if len(removed_tracks) > 0:
         playlist_uris = [uris for _, uris in all_playlists.items()]
         sc.remove_tracks_from_all_playlists(playlist_uris, removed_tracks)
@@ -22,8 +25,11 @@ def sync(sc):
 
 
 def main():
+    dt = datetime.datetime.now()
+    print(f'[{str(dt)}] Starting sync')
     sc = common.get_spotify_client()
     sync(sc)
+    print(f'Finished sync')
 
 
 if __name__ == '__main__':
